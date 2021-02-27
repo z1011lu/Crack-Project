41709cdf3c1c04b9f03f362af90bf8278cd4dc92
Darcy Phipps <diphipps@ucsc.edu>
2021-02-27 14:17:42 -0800

    Working engine

--- display.js
+++ display.js
@@ -1,16 +1,28 @@
+// setup the scene
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75,
-	window.innerWidth/window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, 
+window.innerWidth/window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 camera.position.z = 5;
+camera.position.x = -2
+camera.position.y = 2
 
+//Test cube
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+
+// add the light
 var light = new THREE.PointLight(0xffffff, 1, 1000);
 light.position.set(10, 10, 10);
 scene.add(light);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cube = new THREE.Mesh(geometry, material); 
-scene.add(cube);
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
+animate();
