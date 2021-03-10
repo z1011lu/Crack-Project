47f2e91d8f9220647b0385ec1edd5f61575584a4
Darcy Phipps <diphipps@ucsc.edu>
2021-03-10 13:34:10 -0800

    Made shader changes and started voxels

--- display.js
+++ display.js
@@ -9,20 +9,90 @@ camera.position.z = 5;
 camera.position.x = -2
 camera.position.y = 2
 
-//Test cube
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
-
 // add the light
 var light = new THREE.PointLight(0xffffff, 1, 1000);
 light.position.set(10, 10, 10);
-scene.add(light);
+//scene.add(light);
+
+var geometry1, material1, mesh1;
+
+//the voxels
+let voxelArray = []
+//Initialise all the voxels
+for(let i = 0; i < 100; i++){
+    for(let j = 0; j <100; j++){
+        voxelArray[100*i + j] = 20
+    }
+}
+
+//Initialise 
+
+function addShaderCube() {
+	if(count == 2) {
+  		geometry1 = new THREE.BoxGeometry(1, 1, 1);
+  		var uniforms = {"voxels" : voxelArray};
+  		material1 =  new THREE.ShaderMaterial({
+			        		uniforms: uniforms,
+			  			fragmentShader: fshader,
+			          		vertexShader: vshader,
+			  			precision: "mediump"});
+
+  		mesh1 = new THREE.Mesh(geometry1, material1);
+  		scene.add(mesh1);
+	}
+}
+
+
+
+THREE.Cache.enabled = true;
+var count = 0;
+var loader = new THREE.FileLoader();
+var fshader, vshader;
+
+
+loader.load('shaders/vertexShader.vert',
+    // onLoad callback
+    function (data) {
+        console.log(data); // output the text to the console
+        vshader = data;
+        count += 1;
+        addShaderCube(); // we will write this method
+    },
+    // onProgress callback
+    function (xhr) {
+        console.log((xhr.loaded/xhr.total * 100)+ '% loaded');
+    },
+    // onError callback
+    function (err) {
+        console.error('An error happened');
+    });
+
+loader.load('shaders/fragmentShader.frag',
+    // onLoad callback
+    function (data) {
+        console.log(data); // output the text to the console
+        fshader = data;
+        count += 1;
+        addShaderCube(); // we will write this method
+    },
+    // onProgress callback
+    function (xhr) {
+        console.log((xhr.loaded/xhr.total * 100)+ '% loaded');
+    },
+    // onError callback
+    function (err) {
+        console.error('An error happened');
+    });
+
+var delta = 0.3;
 
 function animate() {
     requestAnimationFrame(animate);
-
     renderer.render(scene, camera);
+    //if (light.position.x > 10 || light.position.x < -10) {
+    //    delta *= -1.0;
+    //} 
+    //light.position.x += delta;
+
 }
 animate();
--- shaders/fragmentShader.frag
+++ shaders/fragmentShader.frag
@@ -1,6 +1,61 @@
-
+uniform sampler3D voxels;
 varying vec2 vUv;
-uniform sampler2D texture1;
+
+/*vec4 ShapeMarch(vec3 p0, vec3 d) {
+  float t = 0;
+  while (t <= maxDistToCheck) {
+    vec3 p = p0 + d * t;
+    uint shapeValue = textureLod(shape, p / voxelGridSize, 0).r;
+    if (shapeValue == 255) {
+      return textureLod(voxels, p / voxelGridSize, 0);
+    }
+    
+    vec3 deltas = (step(0, d) - fract(p)) / d;
+    t += max(mincomp(deltas) + shapeValue, epsilon);
+  }
+
+  return vec4(0);
+}*/
+
+float getDistance(vec3 point) {
+	vec3 spherePosition = vec3(0.0, 2.0, -6.0);
+	float sphereRadius = 1.0; 
+	float sphereDistance = length(point - spherePosition) - sphereRadius;
+	if(int(point.x*100.0+50.0) < 0 || int(point.x*100.0+50.0) >= 100 || int(point.y*100.0+50.0) < 0 || int(point.y*100.0+50.0) >= 100|| int(point.z*100.0+50.0) < 0 || int(point.z*100.0+50.0) >= 100){
+		return (10.0);
+	}
+	float voxelDistance = voxels[int(point.x * 100.0 + point.y)];
+	float planeDistance = point.y;
+
+	return min(planeDistance, voxelDistance);
+}
+
+
+float raymarch(vec3 rayOrigin, vec3 rayDirection) {
+	// implement here
+	int MAX_STEPS = 100;
+	float MAX_DIST = 100.0;
+	float SURFACE_THRESHOLD = .01;
+	float distanceFromOrigin = 0.0;
+	for (int i = 0; i < MAX_STEPS; i++){
+		vec3 point = rayOrigin + distanceFromOrigin * rayDirection;
+		float distanceToScene = getDistance(point);
+		distanceFromOrigin += distanceToScene;
+		if (distanceToScene < SURFACE_THRESHOLD || distanceFromOrigin > MAX_DIST)
+			break;
+	}
+	return distanceFromOrigin;
+}
+
 void main() {
-	gl_FragColor =  texture2D(texture1, vUv);
+    vec3 color = vec3(0.0);
+	vec3 rayOrigin = vec3(0.0, 1.0, 3.0);
+	vec3 rayDirection = normalize(vec3(vUv.x - 0.5, vUv.y - 0.5, -1.0));
+	float distance = raymarch(rayOrigin, rayDirection);
+	vec3 point = rayOrigin + rayDirection * distance;
+	//float diffuse = getLight(point);
+	//color = vec3(diffuse);
+	color = vec3(distance/10.0);
+	gl_FragColor = vec4(color, 1.0);
+
 }
